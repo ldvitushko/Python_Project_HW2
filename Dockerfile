52649cb7af52444403449facc0173349c968e4d8
Lidziya Vitushka <vitushko.l.d@mail.ru>
2021-12-08 09:15:40 +0300

    Fix port

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.8
+EXPOSE 5000
 WORKDIR /Python_Course_HW2
 ADD . /Python_Course_HW2
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def get_weather_data():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
